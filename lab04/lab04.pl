57d29131562e089ae2e48870f3386259d8772c1b
Begu Maria Florina <maria.begu.florina@gmail.com>
2022-03-22 15:12:33 +0200

    Update Tema 4

--- lab04/lab04.pl
+++ lab04/lab04.pl
@@ -3,21 +3,26 @@ language(persana).
 language(greaca).
 language(aramaica).
 
+
+same_person_different_languages(X,Y):-
+	language(X),
+	language(Y),
+	X\==Y.
+
 different_languages(X, Y, Z, T):-
 	language(X),
 	language(Y),
 	language(Z),
 	language(T),
-	X\==Y, X\==Z, X\==T,
-	Y\==Z, Y\==T,
-	Z\==T.
+	X\==Z, X\==T,
+	Y\==Z, Y\==T.
 	
 same_language(X, Y, Z, T):-
 	language(X),
 	language(Y),
 	language(Z),
 	language(T),
-	X\==Y, ((X==Z; X==T);(Y==Z; Y==T)), Z\==T.
+	((X==Z,Y\==T);(X==T, Y\==Z);((Y==Z, X\==T); (Y==T, X\==Z))).
 
 lang_diff(X,Y,Z):-
 	language(X),
@@ -38,6 +43,12 @@ professors_unique(Salal1, Salal2, Atar1, Atar2, Zaman1, Zaman2):-
 
 
 professors(Salal1, Salal2, Atar1, Atar2, Eber1, Eber2, Zaman1, Zaman2 ):-
+
+same_person_different_languages(Salal1,Salal2),
+same_person_different_languages(Atar1,Atar2),
+same_person_different_languages(Eber1,Eber2),
+same_person_different_languages(Zaman1,Zaman2),
+
 Eber1=aramaica,
 
 lang_diff(Zaman1, Zaman2, aramaica),
